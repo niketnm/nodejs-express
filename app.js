e1e2748aba9bc30f04c44f07e5ff0666d25a706b
Malihalli <nmaliha@lowes.com>
2020-04-12 19:59:51 +0530

    Added Logger

--- app.js
+++ app.js
@@ -2,13 +2,19 @@
 const express = require('express');
 //Initialise Chalk
 const chalk = require('chalk');
+//Initialise Debug
+//const debug = require('debug');
+//Initialise Logging
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
     res.send('Hello from Niket');
 });
 
 app.listen(8080, () => {
-    console.log("Server listening on Port 8080");
+    console.log("Server listening on Port " + chalk.red("8000"));
 })
