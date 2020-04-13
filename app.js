55deb5cbdea04c8859d032ad9363be5247aaa23f
Malihalli <nmaliha@lowes.com>
2020-04-13 09:25:41 +0530

    Added css/js referneces

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const express = require('express');
 //Initialise Chalk
 const chalk = require('chalk');
 //Initialise Debug
-//const debug = require('debug');
+const debug = require('debug');
 //Initialise Logging
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
 app.use(morgan('tiny'));
+//app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist/')));
+app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist/')));
+app.use(express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
 app.get('/', (req, res) => {
-    res.send('Hello from Niket');
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(8080, () => {
